590ef5cf54dbd87564ddba521cd13060c32c5fa7
vanshvansh858 <callmevansh858@gmail.com>
2023-11-09 16:28:02 +0530

    forth commit

--- discord-frontend/src/shared/components/InputWithLable.js
+++ discord-frontend/src/shared/components/InputWithLable.js
@@ -1,46 +1,47 @@
-import React from 'react'
-import {styled } from '@mui/system';
+import React from "react";
+import { styled } from "@mui/system";
 
-const Wrapper = styled('div')({
-    display : 'flex',
-    justifyContent : 'center',
-    flexDirection : 'column',
-    width : '100%',
+const Wrapper = styled("div")({
+  display: "flex",
+  justifyContent: "center",
+  flexDirection: "column",
+  width: "100%",
 });
 
-const Lable = styled('div')({
-    color : '#b9bbbe' , 
-    textTransform : 'uppercase',
-    fontWeight : '600',
-    fontSize : '16px',
-    marginTop: '10px',
+const Lable = styled("div")({
+  color: "#b9bbbe",
+  textTransform: "uppercase",
+  fontWeight: "600",
+  fontSize: "16px",
+  marginTop: "10px",
 });
 
-const Input = styled('input')({
-    flexGrow : 1 ,
-    height : '40px',
-    border : '1px solid black',
-    borderRadius  : '5px',
-    background : '#35393f',
-    color : '#dcddde',
-    margin :0,
-    fontSize : "16px",
-    padding : "0 5px",
-
+const Input = styled("input")({
+  flexGrow: 1,
+  height: "40px",
+  border: "1px solid black",
+  borderRadius: "5px",
+  background: "#35393f",
+  color: "#dcddde",
+  margin: 0,
+  fontSize: "16px",
+  padding: "0 5px",
 });
 export const InputWithLable = (props) => {
-
-    const {value , setValue , lable , type , placeholder }= props;
-    const handleValueChange = (event) => {
-        setValue(event.target.value);
-    }
+  const { value, setValue, lable, type, placeholder } = props;
+  const handleValueChange = (event) => {
+    setValue(event.target.value);
+  };
   return (
     <Wrapper>
-        <Lable>
-            {lable}
-        </Lable>
+      <Lable>{lable}</Lable>
 
-        <Input value={value} onChange={handleValueChange} type={type} placeholder={placeholder} />
+      <Input
+        value={value}
+        onChange={handleValueChange}
+        type={type}
+        placeholder={placeholder}
+      />
     </Wrapper>
-  )
-}
+  );
+};
